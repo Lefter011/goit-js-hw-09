77db2d1825cbf25a5591ffde9013cb6aaf90886d
Lefter011 <94438982+Lefter011@users.noreply.github.com>
2022-09-30 08:02:50 +0000

    Deploying to gh-pages from @ Lefter011/goit-js-hw-09@25ce2f1f1b486570fad866f8c3bd3580c03a9c90 🚀

---
+++ 01-color-switcher.6c6ae7e2.js
@@ -0,0 +1,2 @@
+const t={body:document.querySelector("body"),startButton:document.querySelector("button[data-start]"),stopButton:document.querySelector("button[data-stop]")};let e=null;t.stopButton.disabled=!0,t.startButton.addEventListener("click",(o=>{o.target.disabled=!0,t.stopButton.disabled=!1,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopButton.addEventListener("click",(o=>{o.target.disabled=!0,t.startButton.disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.6c6ae7e2.js.map
