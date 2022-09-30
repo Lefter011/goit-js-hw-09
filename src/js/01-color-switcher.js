25ce2f1f1b486570fad866f8c3bd3580c03a9c90
Mike Chernetskyi <michael.chernetskyi@gmail.com>
2022-09-30 11:02:26 +0300

    finnish homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    body: document.querySelector("body"),
+    startButton: document.querySelector("button[data-start]"),
+    stopButton: document.querySelector("button[data-stop]"),
+}
 
+let timerId = null;
+refs.stopButton.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startButton.addEventListener('click', event => {
+    event.target.disabled = true;
+    refs.stopButton.disabled = false;
+
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+})
+
+refs.stopButton.addEventListener('click', event => {
+    event.target.disabled = true;
+    refs.startButton.disabled = false;
+
+    clearInterval(timerId);
+})
